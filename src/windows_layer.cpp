361ac6986ff9f369f3e23782a8932d3337c31ec9
Maciej Oliwa ragnarlodbrok1992 <maraus92@gmail.com>
2025-11-18 21:27:23 +0100

    WIP

--- src/windows_layer.cpp
+++ src/windows_layer.cpp
@@ -55,38 +55,3 @@ extern "C" {
 	}
 }
 
-/*
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	const char CLASS_NAME[] = "Shipment Process";
-
-	WNDCLASS wc = {};
-	wc.lpfnWndProc = WindowProc;
-	wc.hInstance = hInstance;
-	wc.lpszClassName = CLASS_NAME;
-
-	RegisterClass(&wc);
-
-	HWND hwnd = CreateWindowEx(
-			0, CLASS_NAME, "Shipment",
-			WS_OVERLAPPEDWINDOW,
-			CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-			NULL, NULL, hInstance, NULL);
-
-	if (hwnd == NULL) {
-		return 0;
-	}
-
-	// Call to show window - important to see the window!
-	ShowWindow(hwnd, nCmdShow);
-
-	// Msg loop
-	MSG msg = {};
-	while (GetMessage(&msg, NULL, 0, 0)) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-
-	return 0;
-}
-*/
-
